294b47ee8edce5e39aed9923c343ad354107190e
efatuarte <fatuarte.feern21@triana.salesianos.edu>
2021-11-22 14:25:44 +0100

    dtos de security

--- real-estate-security/src/main/java/com/salesianostriana/dam/realestatesecurity/security/dto/LoginDto.java
+++ real-estate-security/src/main/java/com/salesianostriana/dam/realestatesecurity/security/dto/LoginDto.java
@@ -1,4 +1,15 @@
 package com.salesianostriana.dam.realestatesecurity.security.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class LoginDto {
+
+    private String email;
+    private String password;
+
 }
